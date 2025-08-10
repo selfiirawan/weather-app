0725b92284191feb4cf2895b39bf4d44dfc4b6d4
Selfi <celpi.irawan@gmail.com>
2025-08-10 22:39:39 +0800

    complete project

--- weather.js
+++ weather.js
@@ -10,7 +10,8 @@ async function search() {
         const {name, lat, lon, country} = data[i];
         ul.innerHTML += `<li 
                 data-lat="${lat}" 
-                data-lon="${lon}">
+                data-lon="${lon}"
+                data-name="${name}">
                 ${name} <span>${country}</span></li>`;
     }
 }
@@ -19,5 +20,50 @@ const debouncedSearch = _.debounce(() => {
     search();
 }, 600);
 
+async function showWeather(lat, lon, name) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`);
+    const data = await response.json();
+    const temp = Math.round(data.main.temp);
+    const feelslike = Math.round(data.main.feels_like);
+    const humidity = Math.round(data.main.humidity);
+    const wind = Math.round(data.wind.speed);
+    const icon = data.weather[0].icon;
+    document.getElementById('city').innerHTML = name;
+    document.getElementById('degrees').innerHTML = temp + '&deg;C';
+    document.getElementById('feelslikeValue').innerHTML = feelslike + '<span>&#8451;</span>';
+    document.getElementById('windValue').innerHTML = wind + '<span>km/h</span>';
+    document.getElementById('humidityValue').innerHTML = humidity + '<span>%</span>';
+    document.getElementById('icon').src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+    document.querySelector('form').style.display = 'none';
+    document.getElementById('weather').style.display = 'block';
+}
+
 document.querySelector('input[type="text"]')
     .addEventListener('keyup', debouncedSearch);
+
+document.body.addEventListener('click', ev => {
+    const li = ev.target;
+    const {lat, lon, name} = li.dataset;
+    localStorage.setItem('lat', lat);
+    localStorage.setItem('lon', lon);
+    localStorage.setItem('name', name);
+    if (!lat) {
+        return;
+    }
+
+    showWeather(lat, lon, name);
+});
+
+document.getElementById('change').addEventListener('click', () => {
+    document.getElementById('weather').style.display = 'none';
+    document.querySelector('form').style.display = 'block';
+});
+
+document.body.onload = () => {
+    if (localStorage.getItem('lat')) {
+        const lat = localStorage.getItem('lat');
+        const lon = localStorage.getItem('lon');
+        const name = localStorage.getItem('name');
+        showWeather(lat, lon, name);
+    }
+}
